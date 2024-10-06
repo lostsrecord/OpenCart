c234c347983fea0b0acd2f630d269670cbae2c61
lostsrecord <105526090+lostsrecord@users.noreply.github.com>
2024-10-06 12:43:31 -0400

    Update Hello.java with implementations

--- src/test/java/roughWork/Hello.java
+++ src/test/java/roughWork/Hello.java
@@ -4,8 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Our module is complete");
-		
+		System.out.println("Our module is ending in 2 days.");
+		//This class is developed by one of my favorite students
+		//I am optimistic about his or her success
 		
 		
 	}
